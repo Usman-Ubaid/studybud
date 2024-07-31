22bcc39beb052b5b0bf1ff9a4c4f3aca04d1c459
Welcome ! <usmanubaid9@gmail.com>
2024-07-31 16:43:00 +0200

    user registration

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import Room, Topic
@@ -12,7 +13,7 @@ from .forms import RoomForm
 # Create your views here.
 
 def login_page(request):
-
+  page = 'login'
   if request.user.is_authenticated:
     return redirect('home')
 
@@ -33,12 +34,27 @@ def login_page(request):
       return redirect('home')
     else:
       messages.error(request, "Incorrect username or password.")
-  context={}
+  context={"page": page}
   return render(request, 'base/login_register.html',context)
 
 def logout_user(request):
   logout(request)
   return redirect("home")
+
+def register_user(request):
+  form = UserCreationForm()
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.username = user.username.lower()
+      user.save()
+      login(request, user)
+      return redirect("home")
+    else:
+      messages.error(request, "An error occured during registration.")
+
+  return render(request, 'base/login_register.html', {"form":form})
 
 def home(request):
   q = request.GET.get('q') if request.GET.get('q') != None else ''
