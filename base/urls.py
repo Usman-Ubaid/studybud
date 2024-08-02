15a0bc4bc32be3ba180a00abd2fe1bb4d20c91ab
Welcome ! <usmanubaid9@gmail.com>
2024-08-02 16:32:34 +0200

    user profile page

--- base/urls.py
+++ base/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('create-room', views.create_room, name="create-room"),
     path('update-room/<str:pk>/', views.update_room, name="update-room"),
     path('delete-room/<str:pk>/', views.delete_room, name="delete-room"),
+
     path('delete-message/<str:pk>/', views.delete_message, name="delete-message"),
+
+    path('profile/<str:pk>', views.user_profile, name="user-profile")
 ]
